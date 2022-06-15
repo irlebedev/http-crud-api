bdae8b4bbdc209e735d4015e69bdf12b302aa42f
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-15 22:05:05 +0400

    feat: .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import http from "http";
+import "dotenv/config";
 import { router } from "./router/index";
 
 const server = http.createServer((req, res) => {
   router(req, res);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 
 server.listen(PORT, () => {
   console.log("server started at http://localhost:" + PORT);
