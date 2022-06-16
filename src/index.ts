2e6ca0cc146ee7e4f7f883cd9f0c09fb9f766a3a
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-16 17:16:46 +0400

    feat: cluster

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,51 @@
 import http from "http";
+import cluster from "cluster";
+import { cpus } from "os";
 import "dotenv/config";
 import { router } from "./router/index";
 
-const server = http.createServer((req, res) => {
-  router(req, res);
-});
+const isMulti = process.env.MODE === "multi";
+const numCPUs = cpus().length;
 
-const PORT = process.env.PORT || 5555;
+const startServer = () => {
+  const server = http.createServer((req, res) => {
+    logRequest();
+    router(req, res);
+  });
 
-server.listen(PORT, () => {
-  console.log("server started at http://localhost:" + PORT);
-});
+  const PORT = process.env.PORT || 5555;
+
+  server.listen(PORT, () => {
+    console.log("server started at http://localhost:" + PORT);
+  });
+};
+
+if (isMulti) {
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    process.on("SIGINT", () => {
+      cluster.emit("exit");
+    });
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+      worker.disconnect();
+      worker.kill();
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    startServer();
+    console.log(`Worker ${process.pid} started`);
+  }
+} else {
+  startServer();
+}
+
+function logRequest() {
+  if (cluster.isWorker) {
+    console.log(`Worker ${process.pid} handle request`);
+  }
+}
